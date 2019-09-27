6a1b0d51e960f5de261e8b21e4a04010c66cb5ba
czx <chenzhixiang@yit.com>
2019-09-27 10:40:07 +0800

    error捕捉上报

--- src/redux/action/Home.js
+++ src/redux/action/Home.js
@@ -3,44 +3,8 @@ import { FETCH_PRODUCT_LIST } from "../actionTypes";
 
 export const fetchProductList = () => {
   return dispatch => {
-    // console.log(window.error);
-
-    // window.addEventListener("unhandledrejection", (msg) => {
-    //   console.log(msg);
-
-    // });
-
-
-
-    // const b = "qweqweqwe";
-    // Promise.reject(b);
-
-    // setTimeout(() => {
-    //   const test = 123;
-    //   test.forEach(item => {
-    //     console.log(item);
-    //   });
-    // });
-
-    // const test = 123;
-    // test.forEach(item => {
-    //   console.log(item);
-    // });
-
-    // const a = "";
-    // if(!a) {
-    //   throw new Error("errrrrrrorrrr");
-    // }
-
-
-
-
     request({
-      url: "/v1/book",
-      data: {
-        id: 123,
-        type: "abc"
-      }
+      url: "/mock/products/likes.json",
     }).then(res => {
       dispatch({
         type: FETCH_PRODUCT_LIST,
